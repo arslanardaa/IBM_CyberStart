0892233922eca20e8a64b827018edda3538ad8bf
Arda Arslan <133518399+arslanardaa@users.noreply.github.com>
2024-06-28 19:42:17 +0300

    IBM Cybert Start Python : euclideanDistance

---
+++ oklid.py
@@ -0,0 +1,13 @@
+import math
+points=[[(2,2),(5,5)],[(10,10),(1,1)],[(3,4),(5,5)]]
+
+distances = []
+
+def euclideanDistance(nokta):
+    for i in nokta:
+        print(math.sqrt((i[0][0]-i[1][0])**2+(i[0][1]-i[1][1])**2))
+       
+       
+euclideanDistance(points)     
+            
+    
